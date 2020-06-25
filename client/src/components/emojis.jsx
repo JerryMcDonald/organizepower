49a3680043ceb5e4efdddd4f0f67eb5c1e7e7244
JerryMcDonald <jandh62808@gmail.com>
2020-06-25 13:28:07 -0500

    create an emojis component and begin construction

---
+++ client/src/components/emojis.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import EmojiReact from 'react-emoji-react';
+
+
+const emojis = () => {
+  
+  const [emojiList, setemojiList] = useState([
+    {
+      name: 'rage',
+      count: 2
+    },
+    {
+      name: 'blush',
+      count: 1
+    },
+    {
+      name: 100,
+      count: 3
+    },
+    {
+      name: 'grinning',
+      count: 2
+    }
+  ]);
+
+
+
+  
+  
+  
+  return (
+
+  )
+
+}
