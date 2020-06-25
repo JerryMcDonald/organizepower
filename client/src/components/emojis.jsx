936b29252aaf642ecdb1c26438a2d2a223b1763f
JerryMcDonald <jandh62808@gmail.com>
2020-06-25 13:34:44 -0500

    added on Reaction and on EmojiClick

--- client/src/components/emojis.jsx
+++ client/src/components/emojis.jsx
@@ -23,9 +23,27 @@ const emojis = () => {
     }
   ]);
 
+  const onReaction = (name) => {
+    const emojis = emojiList.map(emoji => {
+      if (emoji.name === name) {
+        emoji.count += 1;
+      }
+      return emoji;
+    });
+    setemojiList({ emojis })
+  }
+
+  const onEmojiClick = (name) => {
+    console.log(name);
+    const emojis = emojiList.concat([{name, count: 1}]);
+    setemojiList({ emojis })
+  }
 
 
-  
+
+
+
+
   
   
   return (
