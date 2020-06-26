e641d1733fccafca05e746772fcc80c2c5eefe2d
JerryMcDonald <jandh62808@gmail.com>
2020-06-26 17:54:42 -0500

    created the toggle emojis function and linked emojis component

--- client/src/components/CommentListItem.jsx
+++ client/src/components/CommentListItem.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 import { getUserProfileById } from '../services/services';
+import Emojis from './Emojis.jsx';
 
 // sub component of commentListItem where we recieve a comment
 const CommentListItem = ({ comment, key }) => {
@@ -9,6 +10,7 @@ const CommentListItem = ({ comment, key }) => {
 
   // a user set state for the profile image
   const [user, setUser] = useState({ imageUrl: '' });
+  const [seen, setSeen] = useState(false);
 
   useEffect(() => {
     getUserProfileById(id_user)
@@ -19,14 +21,19 @@ const CommentListItem = ({ comment, key }) => {
       .catch(err => console.error(err));
   }, []);
 
+  function toggleEmoji () {
+    setSeen(!seen);
+    console.log('hello');
+  }
+
   return (
-    <div className="flex items-start px-4 py-6">
+    <div className="flex items-start px-4 py-6 bg-white shadow-lg">
       <img className="w-12 h-12 rounded-full object-cover mr-4 shadow" src={user.imageUrl} alt="avatar" />
       <div className="">
         <div className="flex items-center justify-between" id={key}>
           <h2 className="text-lg font-semibold text-gray-900 -mt-1">{username}</h2>
+          <small className="text-sm text-gray-700 object-left">{moment(createdAt).fromNow()}</small>
         </div>
-        <p className="text-gray-700">{moment(createdAt).fromNow()} - {moment(createdAt).format('dddd, MMMM Do YYYY, h:mm:ss a')}</p>
         <p className="mt-3 text-gray-700 text-sm">
           {commentText}
         </p>
@@ -44,10 +51,8 @@ const CommentListItem = ({ comment, key }) => {
             <span>8</span>
           </div>
           <div className="flex mr-2 text-gray-700 text-sm mr-4">
-            <svg fill="none" viewBox="0 0 24 24" className="w-4 h-4 mr-1" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
-            </svg>
-            <span>reply</span>
+          <button className="modal-open bg-transparent border border-gray-500 hover:border-indigo-500 text-gray-500 hover:text-indigo-500 font-bold py-1 px-2 rounded-full" onClick={toggleEmoji}>+</button>
+            { seen ? <Emojis toggleEmoji={toggleEmoji} /> : null}
           </div>
         </div>
       </div>
@@ -56,3 +61,18 @@ const CommentListItem = ({ comment, key }) => {
 };
 
 export default CommentListItem;
+
+
+{/* <button data-action="increment" class="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer">
+<span class="m-auto text-2xl font-thin">+</span>
+</button>
+
+<svg fill="none" viewBox="0 0 24 24" className="w-4 h-4 mr-1" stroke="currentColor">
+<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
+</svg>
+<span>reply</span>
+
+<Emojis /> */}
+
+
+{/* <button class="modal-open bg-transparent border border-gray-500 hover:border-indigo-500 text-gray-500 hover:text-indigo-500 font-bold py-2 px-4 rounded-full">Open Modal</button> */ }
