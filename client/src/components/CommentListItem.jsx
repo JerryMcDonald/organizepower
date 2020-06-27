7bedebde44cb49c893a763b05e2406b37936bde8
JerryMcDonald <jandh62808@gmail.com>
2020-06-27 18:34:27 -0500

    fixed lint issues and removed pseudocoded funcitons

--- client/src/components/CommentListItem.jsx
+++ client/src/components/CommentListItem.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 import { Emoji } from 'emoji-mart';
+import axios from 'axios';
 import { getUserProfileById } from '../services/services';
 import Emojis from './Emojis.jsx';
-import axios from 'axios';
 
 // sub component of commentListItem where we recieve a comment
 const CommentListItem = ({ comment, key }) => {
@@ -15,9 +15,6 @@ const CommentListItem = ({ comment, key }) => {
   const [seen, setSeen] = useState(false);
   const [emojiArray, setEmojiArray] = useState(JSON.parse(emojiData));
 
-
-
-
   useEffect(() => {
     getUserProfileById(id_user)
       .then(res => {
@@ -27,11 +24,10 @@ const CommentListItem = ({ comment, key }) => {
       .catch(err => console.error(err));
   }, []);
 
-
-  // make a function that will save the current state of the comment into the database on each emoji change
+  // make a function that will save the current state of the comment into the database
   const updateComment = () => {
     // stingify the emojiArray
-    let emojiString = JSON.stringify(emojiArray);
+    const emojiString = JSON.stringify(emojiArray);
     axios.post('/comment/update', { emojiString, id });
   };
 
@@ -47,7 +43,6 @@ const CommentListItem = ({ comment, key }) => {
   //       console.error(err);
   //     });
   // };
-  
   const toggleEmoji = () => {
     setSeen(!seen);
     console.log('hello');
@@ -55,9 +50,9 @@ const CommentListItem = ({ comment, key }) => {
 
   const addToEmojiArray = (emojiObject) => {
     console.log(emojiObject);
-    // make a new array with the added emoji 
-    const newArr = [...emojiArray]
-    newArr.push({ id:emojiObject.id, skin:emojiObject.skin, count: 1});
+    // make a new array with the added emoji
+    const newArr = [...emojiArray];
+    newArr.push({ id: emojiObject.id, skin: emojiObject.skin, count: 1 });
     console.log(newArr);
     setEmojiArray(newArr);
     updateComment();
@@ -73,7 +68,6 @@ const CommentListItem = ({ comment, key }) => {
     updateComment();
   };
 
-
   return (
     <div className="flex items-start px-4 py-6 bg-white shadow-lg">
       <img className="w-12 h-12 rounded-full object-cover mr-4 shadow" src={user.imageUrl} alt="avatar" />
@@ -86,18 +80,16 @@ const CommentListItem = ({ comment, key }) => {
           {commentText}
         </p>
         <div className="mt-4 flex items-center">
-          
-            {emojiArray.map((emoji, index) => (
-              <div className="flex mr-2 text-gray-700 text-sm mr-3">
+          {emojiArray.map((emoji, index) => (
+            <div className="flex mr-2 text-gray-700 text-sm mr-3">
               <button className="modal-open bg-transparent hover:border-indigo-500 text-gray-500 hover:text-indigo-500 font-bold py-1 px-2 rounded-none" style={{ outline: 'none' }} onClick={() => addToEmojiCount(index)}>
                 <Emoji emoji={{ id: emoji.id, skin: emoji.skin }} size={32} />
               </button>
               <div className="flex items-center justify-between" id={key}>
                 <p>{emoji.count}</p>
-              </div> 
               </div>
-            ))}
-          
+            </div>
+          ))}
           <div className="flex mr-2 text-gray-700 text-sm mr-4">
             <button className="modal-open bg-transparent border border-gray-500 hover:border-indigo-500 text-gray-500 hover:text-indigo-500 font-bold py-1 px-2 rounded-full" onClick={toggleEmoji}>+</button>
             {seen ? <Emojis toggleEmoji={toggleEmoji} addToEmojiArray={addToEmojiArray} /> : null}
@@ -109,37 +101,3 @@ const CommentListItem = ({ comment, key }) => {
 };
 
 export default CommentListItem;
-
-
-{/* <button data-action="increment" class="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer">
-<span class="m-auto text-2xl font-thin">+</span>
-</button>
-
-<svg fill="none" viewBox="0 0 24 24" className="w-4 h-4 mr-1" stroke="currentColor">
-<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
-</svg>
-<span>reply</span>
-
-<Emojis /> */}
-
-
-
-{/* <button class="modal-open bg-transparent border border-gray-500 hover:border-indigo-500 text-gray-500 hover:text-indigo-500 font-bold py-2 px-4 rounded-full">Open Modal</button> */ }
-
-// const handleSubmit = (e) => {
-//   e.preventDefault();
-//   // we ill send a body with a movementId, comment and user
-//   axios.post('/comment', { movementId: id, comment: text, authorId: user.id })
-//     .then(() => {
-//       setText('');
-//       // then we will get that data back from the db
-//       axios.get('/comment', { params: { movementId: id } })
-//         .then((response) => {
-//           setComments(response.data);
-//         })
-//         .catch((err) => console.error(err));
-//     })
-//     .catch((err) => console.error(err));
-// };
-
-// const areThereComments = comments.length > 0;
