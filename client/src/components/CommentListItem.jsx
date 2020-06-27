c5ba438b59514e45c5fc5c5d8de964e5ff7e747f
JerryMcDonald <jandh62808@gmail.com>
2020-06-27 14:18:19 -0500

    added the emoji state and a add count funciton

--- client/src/components/CommentListItem.jsx
+++ client/src/components/CommentListItem.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import moment from 'moment';
+import { Emoji } from 'emoji-mart';
 import { getUserProfileById } from '../services/services';
 import Emojis from './Emojis.jsx';
-import { Emoji } from 'emoji-mart'
 
 // sub component of commentListItem where we recieve a comment
 const CommentListItem = ({ comment, key }) => {
@@ -12,6 +12,10 @@ const CommentListItem = ({ comment, key }) => {
   // a user set state for the profile image
   const [user, setUser] = useState({ imageUrl: '' });
   const [seen, setSeen] = useState(false);
+  const [emojiArray, setEmojiArray] = useState([{ id: 'santa', skin: 3, count: 1 }, { id: 'grin', skin: 5, count: 1 }]);
+
+
+
 
   useEffect(() => {
     getUserProfileById(id_user)
@@ -22,10 +26,24 @@ const CommentListItem = ({ comment, key }) => {
       .catch(err => console.error(err));
   }, []);
 
-  function toggleEmoji () {
+  const toggleEmoji = () => {
     setSeen(!seen);
     console.log('hello');
-  }
+  };
+
+  const addToEmojiArray = (emojiObject) => {
+    setEmojiArray(emojiArray.push(emojiObject));
+  };
+
+  const addToEmojiCount = (index) => {
+    console.log(index);
+    // copy the current array of objects
+    let newArr = [...emojiArray];
+    // update the count
+    newArr[index].count += 1;
+    setEmojiArray(newArr)
+  };
+
 
   return (
     <div className="flex items-start px-4 py-6 bg-white shadow-lg">
@@ -39,17 +57,21 @@ const CommentListItem = ({ comment, key }) => {
           {commentText}
         </p>
         <div className="mt-4 flex items-center">
-          <div className="flex mr-2 text-gray-700 text-sm mr-3">
-          <button className="modal-open bg-transparent hover:border-indigo-500 text-gray-500 hover:text-indigo-500 font-bold py-1 px-2 rounded-none" style={{outline: 'none'}} onClick={toggleEmoji}>
-          <Emoji emoji={{ id: 'santa', skin: 3 }} size={32} />
-          </button>
-          <div className="flex items-center justify-between" id={key}>
-            <p>12</p>
-            </div>
-          </div>
+          
+            {emojiArray.map((emoji, index) => (
+              <div className="flex mr-2 text-gray-700 text-sm mr-3">
+              <button className="modal-open bg-transparent hover:border-indigo-500 text-gray-500 hover:text-indigo-500 font-bold py-1 px-2 rounded-none" style={{ outline: 'none' }} onClick={() => addToEmojiCount(index)}>
+                <Emoji emoji={{ id: emoji.id, skin: emoji.skin }} size={32} />
+              </button>
+              <div className="flex items-center justify-between" id={key}>
+                <p>{emoji.count}</p>
+              </div> 
+              </div>
+            ))}
+          
           <div className="flex mr-2 text-gray-700 text-sm mr-4">
-          <button className="modal-open bg-transparent border border-gray-500 hover:border-indigo-500 text-gray-500 hover:text-indigo-500 font-bold py-1 px-2 rounded-full" onClick={toggleEmoji}>+</button>
-            { seen ? <Emojis toggleEmoji={toggleEmoji} /> : null}
+            <button className="modal-open bg-transparent border border-gray-500 hover:border-indigo-500 text-gray-500 hover:text-indigo-500 font-bold py-1 px-2 rounded-full" onClick={toggleEmoji}>+</button>
+            {seen ? <Emojis toggleEmoji={toggleEmoji} /> : null}
           </div>
         </div>
       </div>
@@ -70,6 +92,7 @@ export default CommentListItem;
 <span>reply</span>
 
 <Emojis /> */}
+
 
 
 {/* <button class="modal-open bg-transparent border border-gray-500 hover:border-indigo-500 text-gray-500 hover:text-indigo-500 font-bold py-2 px-4 rounded-full">Open Modal</button> */ }
