1b2c0089d6041be993f1196da6e92259844db321
JerryMcDonald <jandh62808@gmail.com>
2020-06-27 12:25:46 -0500

    added a hard coded emoji on the comment

--- client/src/components/CommentListItem.jsx
+++ client/src/components/CommentListItem.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 import { getUserProfileById } from '../services/services';
 import Emojis from './Emojis.jsx';
+import { Emoji } from 'emoji-mart'
 
 // sub component of commentListItem where we recieve a comment
 const CommentListItem = ({ comment, key }) => {
@@ -39,16 +40,12 @@ const CommentListItem = ({ comment, key }) => {
         </p>
         <div className="mt-4 flex items-center">
           <div className="flex mr-2 text-gray-700 text-sm mr-3">
-            <svg fill="none" viewBox="0 0 24 24" className="w-4 h-4 mr-1" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-            </svg>
-            <span>12</span>
-          </div>
-          <div className="flex mr-2 text-gray-700 text-sm mr-8">
-            <svg fill="none" viewBox="0 0 24 24" className="w-4 h-4 mr-1" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
-            </svg>
-            <span>8</span>
+          <button className="modal-open bg-transparent hover:border-indigo-500 text-gray-500 hover:text-indigo-500 font-bold py-1 px-2 rounded-none" style={{outline: 'none'}} onClick={toggleEmoji}>
+          <Emoji emoji={{ id: 'santa', skin: 3 }} size={32} />
+          </button>
+          <div className="flex items-center justify-between" id={key}>
+            <p>12</p>
+            </div>
           </div>
           <div className="flex mr-2 text-gray-700 text-sm mr-4">
           <button className="modal-open bg-transparent border border-gray-500 hover:border-indigo-500 text-gray-500 hover:text-indigo-500 font-bold py-1 px-2 rounded-full" onClick={toggleEmoji}>+</button>
