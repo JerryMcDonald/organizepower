9e25dd1440d21212587111172344a1fc6cc24abd
JerryMcDonald <jandh62808@gmail.com>
2020-06-28 12:49:16 -0500

    passing the current user into comments

--- client/src/components/CommentList.jsx
+++ client/src/components/CommentList.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import CommentListItem from './CommentListItem.jsx';
 // sub component of Comments.jsx
-const CommentList = ({ comments }) => {
+const CommentList = ({ comments, user }) => {
   return (
     <div className="mt-10">
       <p className="font-bold text-gray-800 text-xl ml-4">Comments</p>
       {/* if there are comments map through them and pass down each comment,
       with he latest comment on top, to commentListItem */}
       {comments.length && comments.map(comment => (
-          <CommentListItem comment={comment} comments={comments} />
+          <CommentListItem comment={comment} comments={comments} currentUser={user} />
       ))}
     </div>
   );
