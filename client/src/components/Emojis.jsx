8364db8a54c090098dcb3341148911be0d721e6b
JerryMcDonald <jandh62808@gmail.com>
2020-06-28 11:34:30 -0500

    removed absolute utility class from emoji modal

--- client/src/components/Emojis.jsx
+++ client/src/components/Emojis.jsx
@@ -6,8 +6,8 @@ import { Picker } from 'emoji-mart';
 const Emojis = ({ toggleEmoji, addToEmojiArray }) => {
   return (
     <Draggable>
-      <div className=" fixed w-full h-full top-0 left-0 flex items-center justify-center">
-        <div className="modal-overlay absolute w-full h-full opacity-50" />
+      <div className="fixed w-full h-full top-0 left-0 flex items-center justify-center">
+        <div className="modal-overlay w-full h-full opacity-50" />
         <div className="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
           <div className="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
             <svg className="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
